34a235710eaf85ac96decf8278e92543f522d569
Sandip Shah <shahsandeep980@gmail.com>
2024-08-03 23:03:28 +0530

    Changed the front end canvas size and added tools

--- public/script.js
+++ public/script.js
@@ -5,17 +5,53 @@ canvas.height = window.innerHeight;
 
 var io = io.connect('http://localhost:8080/')
 
-
 let ctx = canvas.getContext("2d");
-
 let x;
 let y;
-
 let mouseDown = false;
 
+// const tools = document.querySelectorAll('.tools button');
+// let currentTool = 'pencil';
+// // Set up default tool
+// const setTool = (tool) => {
+//     tools.forEach(button => button.classList.remove('selected'));
+//     document.getElementById(tool).classList.add('selected');
+//     currentTool = tool;
+// };
+
+// // Handle tool selection
+// tools.forEach(button => {
+//     button.addEventListener('click', () => {
+//         setTool(button.id);
+//     });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 window.onmousedown = (e) => {
-    ctx.moveTo(x,y);
-    io.emit('down', {x,y});
+    ctx.moveTo(x, y);
+    io.emit('down', { x, y });
     mouseDown = true;
 }
 
@@ -23,22 +59,34 @@ window.onmouseup = (e) => {
     mouseDown = false;
 }
 
-io.on('ondraw', ({x,y}) => {
-    ctx.lineTo(x,y);
+io.on('ondraw', ({ x, y }) => {
+    ctx.lineTo(x, y);
     ctx.stroke();
 })
 
-io.on('ondown', ({x,y}) => {
-    ctx.moveTo(x,y);
+io.on('ondown', ({ x, y }) => {
+    ctx.moveTo(x, y);
 })
 
 window.onmousemove = (e) => {
-    x = e.clientX;
-    y = e.clientY;
+    const rect = canvas.getBoundingClientRect();
+    x = e.clientX - rect.left;
+    y = e.clientY - rect.top;
 
-    if (mouseDown){
-        io.emit('draw', {x,y});
-        ctx.lineTo(x,y);
+    if (mouseDown) {
+        io.emit('draw', { x, y });
+        ctx.lineTo(x, y);
         ctx.stroke();
     }
 };
+
+function resizeCanvas() {
+    canvas.width = canvas.parentElement.clientWidth;
+    canvas.height = canvas.parentElement.clientHeight;
+}
+
+// Initial resize
+resizeCanvas();
+
+// Resize canvas on window resize
+window.addEventListener('resize', resizeCanvas);
